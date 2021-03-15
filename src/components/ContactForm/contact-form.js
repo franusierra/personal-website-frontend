b4be1dfa639ce819e5a964293cf6befb79e6f5f2
Fran Núñez <franusierra@gmail.com>
2021-03-15 14:58:18 +0100

    feat: complete-navigation

--- src/components/ContactForm/contact-form.js
+++ src/components/ContactForm/contact-form.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import './contact-form.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope,faBeer } from "@fortawesome/free-solid-svg-icons";
+
 const ContactForm= (props) =>{ 
     return (
-        <div class="container">
+        <div className="container">
             <form>
-                <h2>Contact Me!</h2>
+                <h2>Let's talk!</h2>
+                <p>If you want to meet up for a <span>Beer <FontAwesomeIcon icon={faBeer}/></span> or talk about 
+                anything you can fill out the form or send an email directly to  
+                <a href="mailto:franusierra@gmail.com"> <FontAwesomeIcon icon={faEnvelope}/> franusierra@gmail.com </a> 
+                and I will answer as fast as possible.</p>
                 <ul>
                     <li>
-                        <label for="name"><span>Name <span class="required-star">*</span></span></label>
+                        <label for="name"><span>Name <span className="required-star">*</span></span></label>
                         <input type="text" id="name" name="user_name"/>
                     </li>
                     <li>
-                        <label for="mail"><span>Email <span class="required-star">*</span></span></label>
+                        <label for="mail"><span>Email <span className="required-star">*</span></span></label>
                         <input type="email" id="mail" name="user_email"/>
                     </li>
                     <li>
